9f6b78d116e9b22e656b58ababa4ca2a2b2bb67e
Aman Kumar <amanjson@gmail.com>
2017-06-29 12:51:12 +0530

    Create cross-tab-session-logout.js

---
+++ cross-tab-session-logout.js
@@ -0,0 +1,70 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+// transfers sessionStorage from one tab to another
+var sessionStorage_transfer = function (event) {
+    if (!event) {
+        event = window.event;
+    } // ie suq
+    if (!event.newValue)
+        return;          // do nothing if no value to work with
+    if (event.key == 'logoutAdmin') {
+
+        sessionStorage.removeItem('SuperAdminLoggedIn');
+        sessionStorage.removeItem('AdminLoggedIn');
+        sessionStorage.removeItem('freight_id');
+        sessionStorage.removeItem('user_details');
+        sessionStorage.removeItem('api_token');
+        sessionStorage.removeItem('user_id');
+        sessionStorage.removeItem('language_id');
+        sessionStorage.removeItem('searchResults');
+        sessionStorage.removeItem('postData');
+        sessionStorage.removeItem('profile_pic');
+        sessionStorage.removeItem('params');
+        sessionStorage.removeItem('stateUrl');
+        sessionStorage.removeItem('state');
+        
+        localStorage.removeItem('logoutAdmin'); // <- could do short timeout as well.
+        
+        location.href = 'http://smartdata.sdd.freightalia.com/admin';
+
+    } else if (event.key == 'logoutClient') {
+
+        sessionStorage.removeItem('userType');
+        sessionStorage.removeItem('ClientLogedIn');
+        sessionStorage.removeItem('api_token');
+        sessionStorage.removeItem('user_id');
+        sessionStorage.removeItem('subDomainparams');
+        sessionStorage.removeItem('subDomainstateUrl');
+        sessionStorage.removeItem('subDomainstate');
+        localStorage.removeItem('searchData');
+
+        localStorage.removeItem('logoutClient');
+
+        location.href = 'http://smartdata.sdd.freightalia.com/#/login';
+
+
+    }
+};
+
+// listen for changes to localStorage
+if (window.addEventListener) {
+    window.addEventListener("storage", sessionStorage_transfer, false);
+} else {
+    window.attachEvent("onstorage", sessionStorage_transfer);
+}
+;
+
+
+// Ask other tabs for session storage (this is ONLY to trigger event)
+if (!sessionStorage.length) {
+    localStorage.setItem('getSessionStorage', 'foobar');
+    localStorage.removeItem('getSessionStorage', 'foobar');
+}
+;
+
+
